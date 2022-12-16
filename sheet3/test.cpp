4cf80c9f884cebb98f8365736e0d87100bd8b90f
eyadfattah23 <eyadabdelfattah4@gmail.com>
2022-12-16 02:22:06 +0200

    com3

---
+++ sheet3/test.cpp
@@ -0,0 +1,25 @@
+#include<iostream>
+#include<algorithm>
+using namespace std;
+
+void fastio()
+{
+   ios_base::sync_with_stdio(false);
+cin.tie(NULL); 
+cout.tie(NULL); 
+}
+
+int main(){
+    fastio();
+
+unsigned int n;
+cin >> n;
+
+for (int i = 1; i <= n; i++)
+{
+    if (n % i == 0)
+        {cout<<i<<endl;}
+}
+
+
+return 0;}
