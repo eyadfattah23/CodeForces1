e2eebb08cf960e95fe1611a58441f9fd195378a7
eyadfattah23 <eyadabdelfattah4@gmail.com>
2023-02-17 18:50:39 +0200

    17/2

--- cont1/eyadio.cpp
+++ cont1/eyadio.cpp
@@ -1,67 +1,36 @@
-#include<bits/stdc++.h>
+#include <bits/stdc++.h>
 using namespace std;
 #define ll long long
 #define all(vec) vec.begin(), vec.end()
 #define rall(vec) vec.rbegin(), vec.rend()
-#define sz(x) int(x. size())
+#define sz(x) int(x.size())
 
 void fastio()
 {
-   ios_base::sync_with_stdio(false);cin.tie(nullptr);cout.tie(nullptr); 
-#ifdef ONLINE_JUDGE
-    freopen("input.txt", "r",stdin);
-    freopen("output.txt", "w",stdout);
-#endif // DEBUG
+    ios_base::sync_with_stdio(false);
+    cin.tie(nullptr);
+    cout.tie(nullptr);
 }
-string decimal_to_binary(int x){
+string decimal_to_binary(int x)
+{
     string binary;
-    while(x> 0){
-    binary += (x % 2) + '0';
-    x/=2;}
-    reverse(all (binary));
-    return binary ;
+    while (x > 0)
+    {
+        binary += (x % 2) + '0';
+        x /= 2;
+    }
+    reverse(all(binary));
+    return binary;
 }
 
-int main(){
+int main()
+{
     fastio();
-   int times =1;
-   // cin >> times;
-   for (int t = 0; t < times; t++)
-   {
-    
+    int times = 1;
+    // cin >> times;
+    for (int t = 0; t < times; t++)
+    {
+    }
 
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-    
-   }
-    
-    
-
-
-
-return 0;}
+    return 0;
+}
